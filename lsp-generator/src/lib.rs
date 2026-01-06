5cdd3a9207d2f142c90f8ac88dd9924cc620927d
Neil Macneale V <neil.macneale.v@gmail.com>
2026-01-06 13:10:56 -0800

    Add the schema for the LSP spec

--- lsp-generator/src/lib.rs
+++ lsp-generator/src/lib.rs
@@ -1,1 +1,24 @@
-pub fn generate() {}
+use crate::spec::Spec;
+
+const URL: &str = "https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/metaModel/metaModel.json";
+
+mod spec;
+
+pub fn generate() {
+  let spec = ureq::get(URL).call().unwrap().into_body().read_json::<Spec>().unwrap();
+
+  dbg!(&spec);
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn reads_spec() {
+    let spec = ureq::get(URL).call().unwrap().into_body().read_json::<Spec>().unwrap();
+
+    dbg!(&spec);
+    panic!();
+  }
+}
---
+++ lsp-generator/src/spec.rs
@@ -0,0 +1,145 @@
+#![allow(dead_code)]
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Spec {
+  pub meta_data:     Metadata,
+  pub requests:      Vec<Request>,
+  pub notifications: Vec<Notification>,
+  pub structures:    Vec<Structure>,
+  pub enumerations:  Vec<Enumeration>,
+  pub type_aliases:  Vec<TypeAlias>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Metadata {
+  pub version: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Request {
+  pub method:            String,
+  pub result:            Type,
+  pub message_direction: MessageDirection,
+  pub params:            Option<Type>,
+  pub partial_result:    Option<Type>,
+  #[serde(default)]
+  pub documentation:     String,
+  #[serde(default)]
+  pub since:             Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Notification {
+  pub method:            String,
+  pub message_direction: MessageDirection,
+  pub params:            Option<Type>,
+  #[serde(default)]
+  pub documentation:     String,
+  #[serde(default)]
+  pub since:             Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Structure {
+  pub name:       String,
+  pub properties: Vec<Property>,
+  #[serde(default)]
+  pub extends:    Vec<Type>,
+  #[serde(default)]
+  pub mixins:     Vec<Type>,
+  #[serde(default)]
+  pub since:      Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Enumeration {
+  pub name:                   String,
+  #[serde(rename = "type")]
+  pub ty:                     Type,
+  #[serde(default)]
+  pub values:                 Vec<EnumValue>,
+  #[serde(default)]
+  pub supports_custom_values: bool,
+  #[serde(default)]
+  pub documentation:          String,
+  #[serde(default)]
+  pub since:                  Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct EnumValue {
+  pub name:  String,
+  pub value: NumberOrString,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+pub enum NumberOrString {
+  Number(i64),
+  String(String),
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TypeAlias {
+  pub name:          String,
+  #[serde(rename = "type")]
+  pub ty:            Type,
+  #[serde(default)]
+  pub documentation: String,
+  #[serde(default)]
+  pub since:         Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum MessageDirection {
+  ClientToServer,
+  ServerToClient,
+  Both,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Property {
+  pub name:          String,
+  #[serde(default)]
+  pub documentation: String,
+  #[serde(rename = "type")]
+  pub ty:            Type,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "kind", rename_all = "camelCase")]
+pub enum Type {
+  Base { name: BaseType },
+  Or { items: Vec<Type> },
+  Literal { value: Literal },
+  StringLiteral { value: String },
+  Reference { name: String },
+  Map { key: Box<Type>, value: Box<Type> },
+  Array { element: Box<Type> },
+  Tuple { items: Vec<Type> },
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Literal {
+  pub properties: Vec<Property>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(untagged)]
+pub enum BaseType {
+  Null,
+  Named(String),
+}
